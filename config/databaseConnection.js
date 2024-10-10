74c3b5bd9b6fcb2f90810e44f2f8744e7e3c9a54
sujith reddy <100951402+sujith1976@users.noreply.github.com>
2024-10-10 18:38:55 +0530

    Create databaseConnection.js

---
+++ config/databaseConnection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require("dotenv");
+
+const dbConnect = () =>  {
+	// Connecting to the database using the provided URL from the environment variables
+	mongoose
+		.connect(process.env.DATABASE_URL, {
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true,
+			
+		})
+		// If the connection is successful, log a success message
+		.then(() => console.log("DB CONNECTION SUCCESS"))
+		// If there are issues connecting to the database, log an error message and exit the process
+		.catch((err) => {
+			console.log(`DB CONNECTION ISSUES`);
+			console.error(err.message);
+			process.exit(1);
+		});
+};
+
+module.exports = dbConnect;
